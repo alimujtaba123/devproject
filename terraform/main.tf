4e765e54e3b32ceb2edc1e282cd88f28ef3b08ae
mujji <alimujtabanaqvi53@gmail.com>
2025-10-24 18:11:03 +0500

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,22 +54,35 @@ resource "aws_instance" "devops_instance" {
     }
     user_data = <<-EOF
                 #!/bin/bash
+                # Update system
                 sudo apt update -y
-                sudo apt install docker.io -y
-                sudo systemctl start docker
+                sudo apt upgrade -y
+
+                # Install Docker and Git
+                sudo apt install -y docker.io git
                 sudo systemctl enable docker
+                sudo systemctl start docker
                 sudo usermod -aG docker ubuntu
-            
-                sudo apt install -y openjdk-17-jre
-                curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+
+                # Install Java
+                sudo apt install -y fontconfig openjdk-17-jre gnupg2 curl
+
+                # Add Jenkins key and repo (for Ubuntu 24.04+)
+                sudo mkdir -p /usr/share/keyrings
+                curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
                     /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
                 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
                     https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
                     /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+                # Install Jenkins
                 sudo apt update -y
                 sudo apt install -y jenkins
+
+                # Enable and start Jenkins
                 sudo systemctl enable jenkins
-                sudo systemctl start jenkins
+                sudo systemctl start jenkins            
                 EOF
 }
 
